54580c86127b9e5c0826f1cc8dbacb66fcd42575
igor <havinson1988@gmail.com>
2018-07-28 06:34:07 +0300

    All DBDAO are finished. CreateDB.sql file is fixed. Added some fixes in beans files.

--- megaByte/src/dao/EventDBDAO.java
+++ megaByte/src/dao/EventDBDAO.java
@@ -34,17 +34,18 @@ public class EventDBDAO implements EventDAO {
 		try {
 			Connection conn = _pool.getConnection();
 			PreparedStatement statement = conn.prepareStatement(
-					"isert into Event (Name, Id, Date, Status, Type, Place, Price, Participants_quantity, Organizer) values (?,?,?,?,?,?,?,?,?); ");
+					"insert into Event (Name, Id, Date, Status, Type, Place, Price, Participants_quantity, Organizer) values (?, ?, ?, ?, ?, ?, ?, ?, ?);");
 			statement.setString(1, event.get_name());
 			statement.setLong(2, event.getId());
 			statement.setDate(3, new Date(event.get_date().getTime()));
-			statement.setString(4, event.get_status().name());
-			statement.setString(5, event.get_type().name());
+			statement.setString(4, event.get_status().toString());
+			statement.setString(5, event.get_type().toString());
 			statement.setString(6, event.get_place());
 			statement.setDouble(7, event.get_price());
 			statement.setInt(8, event.get_participantsQuantity());
 			statement.setString(9, event.get_organizer().toString());
 			statement.executeUpdate();
+			statement.close();
 			_pool.returnConnection(conn);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
@@ -60,6 +61,7 @@ public class EventDBDAO implements EventDAO {
 			PreparedStatement statement = conn.prepareStatement("delete from Event where Id = ?;");
 			statement.setLong(1, event.getId());
 			statement.executeUpdate();
+			statement.close();
 			_pool.returnConnection(conn);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
@@ -84,6 +86,7 @@ public class EventDBDAO implements EventDAO {
 			statement.setString(8, event.get_organizer().toString());
 			statement.setLong(9, event.getId());
 			statement.executeUpdate();
+			statement.close();
 			_pool.returnConnection(conn);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
@@ -105,6 +108,8 @@ public class EventDBDAO implements EventDAO {
 					new Date(result.getDate("Date").getTime()), EventStatus.valueOf(result.getString("Status")),
 					EventType.valueOf(result.getString("Type")), result.getString("Place"), result.getDouble("Price"),
 					result.getInt("Participants_quantity"), _organizerDao.getOrganizer(result.getLong("Organizer")));
+			result.close();
+			statement.close();
 			_pool.returnConnection(conn);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
@@ -121,7 +126,7 @@ public class EventDBDAO implements EventDAO {
 		try {
 			Connection conn = _pool.getConnection();
 			Statement statement = conn.createStatement();
-			ResultSet result = statement.executeQuery("select * from Event");
+			ResultSet result = statement.executeQuery("select * from Event;");
 			while (result.next()) {
 				events.add(new Event(result.getLong("Id"), result.getString("Name"),
 						new Date(result.getDate("Date").getTime()), EventStatus.valueOf(result.getString("Status")),
@@ -129,14 +134,15 @@ public class EventDBDAO implements EventDAO {
 						result.getDouble("Price"), result.getInt("Participants_quantity"),
 						_organizerDao.getOrganizer(result.getLong("Organizer"))));
 			}
-
+			result.close();
+			statement.close();
 			_pool.returnConnection(conn);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return null;
+		return events;
 	}// getAllEvents
 
 	@Override
